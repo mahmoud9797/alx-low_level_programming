5183503735e8e5b5aadedf7467951c33aa9540df
Mahmoud <elmahallawy6@gmail.com>
2023-07-13 19:46:26 -0700

    mahmoud

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- *main -Entry point
- *Description : AC program with put function
- *Return: Always 0 (succuess)
-*/
-
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
 }
