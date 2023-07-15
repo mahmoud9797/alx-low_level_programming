ec3184938bff195729273ca5dffd472d921eaa98
Mahmoud <elmahallawy6@gmail.com>
2023-07-15 12:54:43 -0700

    mahmoud

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,11 @@
  */
 int main(void)
 {
-        char ch = "a";
+	char c;
 
-        while (ch <= "z")
-        {
-                puutchar(ch);
-                ch++;
-        }
-        putchar("\n")
-        return (0)
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
 
+	putchar('\n');
+	return (0);
 }
