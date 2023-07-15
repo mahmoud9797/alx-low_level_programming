5328f733880d15e4939b79e94590d499fbfa90d3
Mahmoud <elmahallawy6@gmail.com>
2023-07-15 12:30:54 -0700

    mahmoud

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char ch = "a";
+
+	while (ch <= "z")
+	{
+		puutchar(ch);
+		ch++;
+	}
+	putchar("\n")
+	return (0)
+
+}
