3c5dcad71938d945f428a56b13daecca6ed5c1d1
Mahmoud <elmahallawy6@gmail.com>
2023-07-16 10:54:28 -0700

    mahmoud

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+  * main - Prints combination of numbers
+  *
+  * Return: Always (Success)
+*/
+int main(void)
+{
+	int n1 = '0', n2;
+
+	while (n1 <= '9')
+	{
+		n2 = '0'
+		while (n2 <= '9')
+		{
+			if (n1 != n2 && n1 < n2)
+			{
+				putchar(n1);
+				putchar(n2);
+
+				if (n1 + n2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n2++;
+		}
+		n1++;
+	}
+	putchar('\n');
+	return (0);
+}
