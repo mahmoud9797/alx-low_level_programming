fe6a710edfba0de8b9dc204fcceecfb98469c614
Mahmoud <elmahallawy6@gmail.com>
2023-07-16 07:47:52 -0700

    mahmoud

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print number fron 0 to 9 seprated by ',' and ' '
+ *
+ *
+ *Return: Always success
+*/
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		if (n != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		n++
+	}
+	putchar(\n);
+	return (0);
+}
