96c41f7489088fa3f6541a366a1b05d2986566f8
Mahmoud <elmahallawy6@gmail.com>
2023-07-19 01:56:41 -0700

    mahmoud

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_isalpha - Checks if a character is alphabetic.
+ *@c: the charcter to be checked
+ *Return: 1 if the charcter is alphabet
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+int _isalpha(int c);
 void print_alphabet(void);
 int _islower(int c);
 int _putchar(char c);
