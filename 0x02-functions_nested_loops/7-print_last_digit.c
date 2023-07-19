c90f188fa3ce0475b6f880e3b171539a83550fa8
Mahmoud <elmahallawy6@gmail.com>
2023-07-19 06:35:03 -0700

    mahmoud

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
  */
 int print_last_digit(int n)
 {
-	int l;
+	int last_digit = n % 10;
 
-	if (n < 0)
-		l = -1 * (n % 10);
-	else
-		l = n % 10;
-	putchar(l + '0');
-	return (l);
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
