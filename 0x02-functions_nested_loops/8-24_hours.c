fd5005fee022dc4965203bcc17c5fdc514f869ad
Mahmoud <elmahallawy6@gmail.com>
2023-07-19 08:28:00 -0700

    aaa

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ void jack_bauer(void)
 	{
 		for (min = 0; min <= 59; min++)
 		{
-			_putchar((h / 10) + 48);
+			putchar((h / 10) + 48);
 			_putchar((h % 10) + 48);
 			_putchar(':');
 			_putchar((min / 10) + 48);
