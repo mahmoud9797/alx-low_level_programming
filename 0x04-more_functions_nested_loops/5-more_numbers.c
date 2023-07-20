d79c777619d94807cd445c75a81b7e52735527eb
Mahmoud <elmahallawy6@gmail.com>
2023-07-20 08:08:16 -0700

    mahmoud

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *more_numbers - function to print numbers from 0 to 14
+*/
+
+void more_numbers(void)
+{
+	int r, n;
+
+	for (r = 1; r <= 10; r++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+				_putchar((a / 10) + 48);
+			_putchar((a % 10) + 48);
+			else
+			{
+				_putchar(a + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
