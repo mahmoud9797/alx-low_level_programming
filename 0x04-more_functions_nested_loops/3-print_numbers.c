81207b50c555e68145779f9cea518987566033ba
Mahmoud <elmahallawy6@gmail.com>
2023-07-20 06:06:20 -0700

    mahmoud

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_numbers - function which print numbers from 0 to 9
+ *
+ *c: is the number will be printed
+ *
+*/
+
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+}
