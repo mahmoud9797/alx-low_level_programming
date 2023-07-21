2e53de19a739b4a5bd426cebe381a45588c1daba
Mahmoud <elmahallawy6@gmail.com>
2023-07-20 18:43:05 -0700

    mahmoud

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,13 @@ void print_line(int n)
 {
 	int a;
 
-	if (n > 0)
+	if (n < 0)
 	{
-		for (a = 0; a <= n; a++)
+		for (a = 1; a <= n; n++)
+		{
 			_putchar('_');
 		_putchar('\n');
+		}
 	}
 	else
 		_putchar('\n');
