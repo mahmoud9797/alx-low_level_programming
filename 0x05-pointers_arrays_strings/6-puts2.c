d9e52e4df717725d28a2919afc05aa3bf7a50b06
Mahmoud <elmahallawy6@gmail.com>
2023-07-24 13:01:13 -0700

    mahmoud

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' ; i += 2)
+	{
+		_puutchar(str[i]);
+	}
+	_putcahr('\n');
+}
