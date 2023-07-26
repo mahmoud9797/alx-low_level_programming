0e19427057dee98ec57773f244a554c6be34a16b
Mahmoud <elmahallawy6@gmail.com>
2023-07-25 19:32:27 -0700

    mahmoud

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -10,7 +10,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 int _atoi(char *s);
 void print_array(int *a, int n);
-
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
 
 
 
