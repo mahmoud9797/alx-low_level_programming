325155407d0c0b915fc92e2162b353ace0e04e7c
Mahmoud <elmahallawy6@gmail.com>
2023-07-30 11:56:04 -0700

    mahmoud

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * string_toupper - Changes all lowercase letters to uppercase
+  * @s: The string will be modified
+  *
+  * Return: char var
+  */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (i <= 122 && i >= 97)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (0);
+}
