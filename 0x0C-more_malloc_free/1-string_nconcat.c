a519e3ed3b33be167124058fa3a3088d6e3e8314
Mahmoud <elmahallawy6@gmail.com>
2023-08-10 19:45:15 -0700

    mahmoud

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *m;
-	int i, j, len1, len2;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 	{
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != NULL; i++)
 		m[i] = s1[i];
 	for (j = 0; j < n; j++)
-		m[i++] = s1[l];
+		m[i++] = s1[j];
 	m[i] = NULL;
 	return (m);
 }
