0ef318eb7094011623afb6fd7d52e8b505be41f6
Mahmoud <elmahallawy6@gmail.com>
2023-08-28 19:08:57 -0700

    mahmoud

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,10 +10,10 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
-	listint_t *temp;
+	const listint_t *temp;
 	
 	temp = h;
-	while (h != NULL)
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		num++;
